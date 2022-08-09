55f2f1019d8abaf810656a4eebf2e6c6da6c1c76
powd <powdgit@gmail.com>
2022-08-09 03:55:10 +0200

    correct test

--- src/lib/common/generate-id.spec.ts
+++ src/lib/common/generate-id.spec.ts
@@ -15,16 +15,21 @@ type CryptoWithSelectedOverloads = Omit<typeof crypto, 'randomInt'> & {
 
 const mockedCrypto = crypto as any as jest.Mocked<CryptoWithSelectedOverloads>;
 
-const pseudorandomIntegers = [
-  18, 15, 41, 6, 30, 13, 13, 52, 13, 51, 14, 38, 30, 17, 52, 30, 33, 40, 53, 20,
-  41, 2, 52, 22, 29, 6, 24, 43, 34, 4, 9, 32, 41, 21, 51, 56, 41, 30, 4, 28, 7,
-  13, 12, 17, 12, 47, 47, 33, 31, 37, 25, 22, 44, 33, 37, 36, 5, 15, 14, 16, 24,
-  50, 14, 35, 51, 45, 19, 36, 48, 0, 1, 13, 39, 36, 9, 46, 30, 43, 49, 6, 38, 9,
-  24, 56, 13, 12, 28, 18, 10, 42, 17, 25, 25, 10, 17, 2, 22, 47, 19, 7,
-];
-
 describe('generateId', () => {
   function mockReturnValuesForRandomInt() {
+    const pseudorandomIntegers = [
+      18, 15, 41, 6, 30, 13, 13, 52, 13, 51, 14, 38, 30, 17, 52, 30, 33, 40, 53,
+      20, 41, 2, 52, 22, 29, 6, 24, 43, 34, 4, 9, 32, 41, 21, 51, 56, 41, 30, 4,
+      28, 7, 13, 12, 17, 12, 47, 47, 33, 31, 37, 25, 22, 44, 33, 37, 36, 5, 15,
+      14, 16, 24, 50, 14, 35, 51, 45, 19, 36, 48, 0, 1, 13, 39, 36, 9, 46, 30,
+      43, 49, 6, 38, 9, 24, 56, 13, 12, 28, 18, 10, 42, 17, 25, 25, 10, 17, 2,
+      22, 47, 19, 7,
+    ];
+    expect(
+      pseudorandomIntegers.every(
+        (p) => MINIMAL_RANDOM_INT <= p && p < MAXIMAL_RANDOM_INT,
+      ),
+    ).toEqual(true);
     pseudorandomIntegers.forEach((integer) =>
       mockedCrypto.randomInt.mockReturnValueOnce(integer),
     );
@@ -33,11 +38,6 @@ describe('generateId', () => {
   it('should use correctly tweaked parameters', () => {
     // Then
     expect(ID_LENGTH).toBeGreaterThan(0);
-    expect(
-      pseudorandomIntegers.every(
-        (p) => MINIMAL_RANDOM_INT <= p && p < MAXIMAL_RANDOM_INT,
-      ),
-    ).toEqual(true);
     expect(MINIMAL_RANDOM_INT).toBeLessThanOrEqual(MAXIMAL_RANDOM_INT);
     expect(ALLOWED_CHARACTERS.length).toBe(MAXIMAL_RANDOM_INT);
   });
